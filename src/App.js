0772b3a051a409fd19aadb373dabd868b50202f5
Jose <arunx.jose@intel.com>
2020-11-17 14:53:47 +0530

    created homepage for ecommerce app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Homepage from "./components/homepage/homepage.components"
+
+function App() {
+  return (
+    <div className="App">
+      hello world
+      <Homepage></Homepage>
+    </div>
+    
+    
+  );
+}
+
+export default App;
