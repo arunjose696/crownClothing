2d71a4feba004af3203193b8a717ea7f99ef6460
Jose <arunx.jose@intel.com>
2020-11-18 23:55:21 +0530

    split app to menu item and directories

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
-import Homepage from "./components/homepage/homepage.components"
+import Homepage from "./pages/homepage/homepage.components"
 
 function App() {
   return (
     <div className="App">
-      hello world
+      
       <Homepage></Homepage>
     </div>
     
---
+++ src/pages/homepage/homepage.components.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import "./homepage.styles.scss"
+import {Directories} from "../../components/directories/directories.components"
+
+const Homepage = () => {
+    return (
+        <div className="homepage">
+            
+            
+                <Directories></Directories>
+                
+            
+        </div>
+    )
+
+
+}
+
+export default Homepage;
